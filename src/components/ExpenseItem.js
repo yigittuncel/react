f588223c19e7652068ac7d473d81c44154e3d163
yigit tuncel <yigittuncel35@gmail.com>
2022-03-22 17:19:26 +0100

    ExpenseItem css style

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>March 28th 2021</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$200</div>
+        <div className='expense-item__price'>$203.99</div>
       </div>
     </div>
   )
