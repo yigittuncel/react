cfbcfa21692fdf02300b24858b70208366d521a5
yigit tuncel <yigittuncel35@gmail.com>
2022-04-04 16:12:52 +0200

    building custom context provider component

--- Login/src/components/Login/Login.js
+++ Login/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -26,7 +27,7 @@ const passwordReducer = (state, action) => {
   return {value: '', isValid: false}
 }
 
-const Login = (props) => {
+const Login = () => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -36,9 +37,10 @@ const Login = (props) => {
   const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: null});
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: null});
 
+  const authCtx = useContext(AuthContext);
+
   const {isValid: emailIsValid} = emailState;
   const {isValid: passwordIsValid} = passwordState;
-
 
   useEffect(() => {
     const identifier = setTimeout(() => {
@@ -77,7 +79,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
