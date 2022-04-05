600d147e981c7f7fa175c17fdb1ec30535754df1
yigit tuncel <yigittuncel35@gmail.com>
2022-04-05 17:58:04 +0200

    working with forwarded refs

--- FoodOrder/src/components/Meals/MealItem/MealItem.js
+++ FoodOrder/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,23 @@
+import { useContext } from 'react'
+import CartContext from '../../../store/cart-context'
+
 import classes from './MealItem.module.css'
 import MealItemForm from './MealItemForm'
 
 const MealItem = props => {
+  const cartCtx = useContext(CartContext)
+  console.log(cartCtx);
+
   const price = `$${props.price.toFixed(2)}`
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    })
+  }
 
   return (
     <li className={classes.meal}>
@@ -12,7 +27,7 @@ const MealItem = props => {
         <p className={classes.price}>{price}</p>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   )
