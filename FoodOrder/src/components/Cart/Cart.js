044e1fcd9b5424f77772fb9506494bd35deb4d37
yigit tuncel <yigittuncel35@gmail.com>
2022-04-05 15:31:53 +0200

    Adding Modal via React Portal

--- FoodOrder/src/components/Cart/Cart.js
+++ FoodOrder/src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
 
 const Cart = props => {
   const cartItems = <ul className={classes['cart-items']}>{[
@@ -7,7 +8,7 @@ const Cart = props => {
 
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -17,7 +18,7 @@ const Cart = props => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 
