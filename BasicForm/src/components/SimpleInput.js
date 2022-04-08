0777a09387d24fd027a85c8e107369089ef21ec2
yigit tuncel <yigittuncel35@gmail.com>
2022-04-08 17:53:04 +0200

    refactoring & deriving states

--- BasicForm/src/components/SimpleInput.js
+++ BasicForm/src/components/SimpleInput.js
@@ -1,12 +1,18 @@
-import { useState} from 'react'
+import { useState } from 'react'
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
+  }
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
   }
 
   const formSubmissionHandler = event => {
@@ -14,22 +20,27 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false)
+    if (!enteredNameIsValid) {
       return;
-    }
+    };
 
-    setEnteredNameIsValid(true);
     setEnteredName('');
+    setEnteredNameTouched(false);
   }
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
 
   return (
     <form  onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
+        <input
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
         {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
