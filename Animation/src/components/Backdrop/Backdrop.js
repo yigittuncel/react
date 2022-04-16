6802a24c824b8d67c034ce9137862dc4219dd0a7
yigit tuncel <yigittuncel35@gmail.com>
2022-04-16 15:20:18 +0200

    css animations

--- Animation/src/components/Backdrop/Backdrop.js
+++ Animation/src/components/Backdrop/Backdrop.js
@@ -4,7 +4,6 @@ import './Backdrop.css';
 
 const backdrop = (props) => {
   const cssClasses = ["Backdrop", props.show ? "BackdropOpen" : "BackdropClose"]
-  console.log(cssClasses.join(' '));
 
   return (
     <div className={cssClasses.join(' ')}></div>
